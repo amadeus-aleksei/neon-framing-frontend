aac61b7682f75880dd3dc777fcd55bb93888e48a
Amadeus Aleksei <amadeusaleksei@gmail.com>
2025-05-20 06:47:32 -0500

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/common/Header'
+import Hero from './components/sections/Hero'
 import './styles/styles.scss'
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Hero />
     </div>
   )
 }
---
+++ src/components/sections/Hero.jsx
@@ -0,0 +1,12 @@
+// Hero Component
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Bring Your Vision to Life</h1>
+      <p>We design and develop stunning websites that captivate your audience.</p>
+      <button>Get Started</button>
+    </section>
+  );
+}
+
+export default Hero
