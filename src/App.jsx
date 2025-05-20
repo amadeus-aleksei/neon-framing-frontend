301d23c76951aef61aadbcfadff1d5a4483420a6
Amadeus Aleksei <amadeusaleksei@gmail.com>
2025-05-20 06:10:18 -0500

    Add codebase changes and Header -Added sections folder under components -Updated meta title tag -Added Header.jsx -Added body and .header to styles.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './components/common/Header'
 import './styles/styles.scss'
 
 function App() {
   return (
-    <>
-    </>
+    <div className='App'>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo">Neon Framing</div>
+      <nav className="nav">
+        <ul>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
